cd351ff3ebd3c3d7a48cf1a70755f6ea60ca8f2f
Liam McMurray <liammcmurray@gmail.com>
2019-02-05 10:44:57 +0000

    split structure into registration and renewals

--- app/routes.js
+++ app/routes.js
@@ -6,11 +6,9 @@ router.post('/renewals-check', function (req, res) {
   let renewalsChoice = req.session.data['wasteExemptionOptions']
 
   if (renewalsChoice === 'start') {
-    res.redirect('/start')
+    res.redirect('/registration/start')
   } else if (renewalsChoice === 'renew') {
-    res.redirect('/renew')
-  } else if (renewalsChoice === 'change') {
-    res.redirect('/change')
+    res.redirect('renewals/start')
   } else {
     res.redirect('/renewals-error')
   }
@@ -22,19 +20,19 @@ router.post('/renew-check', function (req, res) {
   var regNumber = (regNum.toUpperCase());
 
   if (regNumber === '') {
-    res.redirect('/renew-no-number')
+    res.redirect('/renewals/renew-no-number')
   } else if (regNumber === 'WEX123452') {
-    res.redirect('/renew-not-ready-error')
+    res.redirect('/renewals/renew-not-ready-error')
   } else if (regNumber === 'WEX123454') {
-    res.redirect('/renew-not-recognised')
+    res.redirect('/renewals/renew-not-recognised')
   } else if (regNumber === 'WEX123456') {
-    res.redirect('/check-email')
+    res.redirect('/renewals/check-email')
   } else if (regNumber === 'WEX123457') {
-    res.redirect('/renew-need-new-error')
+    res.redirect('/renewals/renew-need-new-error')
   } else if (regNumber === 'WEX123458') {
-    res.redirect('/renew-need-new')
+    res.redirect('/renewals/renew-need-new')
   } else {
-    res.redirect('/renew-wrong-number')
+    res.redirect('/renewals/renew-wrong-number')
   }
 })
 
@@ -42,11 +40,11 @@ router.post('/send-email-check', function (req, res) {
   let emailAddress = req.session.data['emailCorrect']
 
   if (emailAddress === 'yes') {
-    res.redirect('/send-email')
+    res.redirect('/renewals/send-email')
   } else if (emailAddress === 'no') {
     res.redirect('/change')
   } else {
-    res.redirect('/check-email-error')
+    res.redirect('/renewals/check-email-error')
   }
 })
 
@@ -56,9 +54,9 @@ router.post('/check-details-check', function (req, res) {
   if (confirmDetails === 'yes') {
     res.redirect('/declaration')
   } else if (confirmDetails === 'no') {
-    res.redirect('/details-need-new')
+    res.redirect('/renewals/details-need-new')
   } else {
-    res.redirect('/check-details-error')
+    res.redirect('/renewals/check-details-error')
   }
 })
 
