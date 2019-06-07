e71b01e2b0433738cf8ed3efb3656998c93f9445
Martin Glancy <martin.glancy@environment-agency.gov.uk>
2019-06-07 14:52:02 +0100

    Journey up to contact address

--- app/routes_registration_v1.js
+++ app/routes_registration_v1.js
@@ -1,22 +1,53 @@
 const express = require('express')
 const router = express.Router()
 
-var folder = "registration-v1"
+var folder = "/registration-v1/"
 
-// RENEWALS
+router.use(function (req, res, next) {
+  // set a folder and store in locals
+  // this can then be used in pages as {{folder}}
+  res.locals.folder=folder
+  next()
+});
+
+// Start new, re-reg or change first page
 router.post('/start-check', function (req, res) {
   let registerChoice = req.session.data['registerChoice']
 
   if (registerChoice === 'startNew') {
-    res.redirect('xxxxxx')
+    res.redirect(folder + 'country')
   } else if (registerChoice === 'reRegister') {
-    res.redirect('xxxxx')
+    res.redirect(folder + '/country')
   } else if (registerChoice === 'change') {
-    res.redirect('xxxxx')
+    res.redirect(folder + '/contact-ea')
   } else {
-    res.redirect('xxxxxx')
+    res.redirect(folder + '/start')
   }
 })
+
+// Country check
+router.post('/country-check', function (req, res) {
+  let countryChoice = req.session.data['countryChoice']
+
+  if (countryChoice === 'England') {
+    res.redirect(folder + 'app-contact-name')
+  } else {
+    res.redirect(folder + 'country-not-england')
+  }
+})
+
+// Operator type check
+router.post('/operator-type-check', function (req, res) {
+  let operatorType = req.session.data['operatorType']
+
+  if (operatorType === 'Limited company') {
+    res.redirect(folder + 'op-limited-company-reg-number')
+  } else {
+    res.redirect(folder + 'op-type-not-covered')
+  }
+})
+
+
 
 
 
