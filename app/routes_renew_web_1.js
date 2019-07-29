4ba619a276a5a52f5fdf0e2b2b8a5aa955ea4d72
Martin Glancy <martin.glancy@environment-agency.gov.uk>
2019-07-29 11:35:09 +0100

    Add sample data for web renewal

--- app/routes_renew_web_1.js
+++ app/routes_renew_web_1.js
@@ -10,6 +10,145 @@ router.use(function (req, res, next) {
   next()
 });
 
+var samples = {}
+
+// Sample data: company
+samples.WEX339257 = {
+  registerChoice:'reRegister',
+  regNumber:'WEX339257',
+  expiryDate:'20 August 2019',
+  countryChoice:'',
+  appFirstName:'',
+  appLastName:'',
+  appTelephoneNumber:'',
+  appEmail:'',
+  appEmailConfirm:'',
+  operatorType:'Limited company',
+  opLtdCompanyRegNumber:'AB123456',
+  opLtdCompanyName:'Farm Techniques Ltd',
+  opLtdCompanyPostcode:'BS4 5FT',
+  opLtdCompanyAddress:'WEEB WAY, COLLSTON, TREWBRIDGE',
+  opFirstName:'',
+  opLastName:'',
+  opContactPosition:'',
+  opContactTelephone:'',
+  opContactEmail:'',
+  opContactEmailConfirm:'',
+  opContactPostcode:'',
+  opContactAddress:'',
+  isFarm:'',
+  isFarmer:'',
+  siteGridRef:'ST 58132 72695',
+  siteAddress:'WEEB WAY, COLLSTON, TREWBRIDGE',
+  siteDescription:'Lower field, Oak Tree Farm. Parcel ED/1234',
+  siteAddressNumber: 'WEEB WAY',
+  siteAddress1: 'COLLSTON',
+  siteAddress2: '',
+  siteTown: 'TREWBRIDGE',
+  sitePostcode: 'TA17 5RF',
+  Exemptions: [
+    'U1',
+    'U8',
+    'T1',
+    'D7',
+    'S2',
+    'S3'
+  ]
+}
+
+// INDIVIDUAL
+samples.WEX354267 = {
+  registerChoice:'reRegister',
+  regNumber:'WEX354267',
+  expiryDate:'20 August 2019',
+  countryChoice:'',
+  appFirstName:'',
+  appLastName:'',
+  appTelephoneNumber:'',
+  appEmail:'',
+  appEmailConfirm:'',
+  operatorType:'Individual or sole trader',
+  individualName:'Sarah Williams',
+  individualPostcode:'BD45 3UY',
+  individualAddress:'THE OLD FARM, TRUSCOTT, BEDFORDSHIRE',
+  opFirstName:'Sarah',
+  opLastName:'Williams',
+  opContactPosition:'',
+  opContactTelephone:'',
+  opContactEmail:'',
+  opContactEmailConfirm:'',
+  opContactPostcode:'',
+  opContactAddress:'',
+  isFarm:'',
+  isFarmer:'',
+  siteGridRef:'ST5320762142',
+  siteDescription:'',
+  siteAddress:'THE OLD FARM, TRUSCOTT, BEDFORDSHIRE',
+  siteAddressNumber: 'THE OLD FARM',
+  siteAddress1: 'TRUSCOTT',
+  siteAddress2: '',
+  siteTown: 'BEDFORDSHIRE',
+  sitePostcode: 'BD45 3UY',
+  Exemptions: [
+    'U1',
+    'U2',
+    'U4',
+    'U8',
+    'U10',
+    'U13',
+    'T1',
+    'D1',
+    'D7',
+    'S2'
+  ]
+}
+
+// PARTNERSHIP
+samples.WEX432123 = {
+  registerChoice:'reRegister',
+  regNumber:'WEX432123',
+  expiryDate:'20 August 2019',
+  countryChoice:'',
+  appFirstName:'',
+  appLastName:'',
+  appTelephoneNumber:'',
+  appEmail:'',
+  appEmailConfirm:'',
+  operatorType:'Partnership',
+  opPartnershipName: 'Brian Purdy',
+  opPartner1FirstName:'Brian',
+  opPartner1LastName:'Purdy',
+  opPartner2FirstName:'Jane',
+  opPartner2LastName:'Purdy',
+  individualName:'Brian Purdy',
+  opPartnershipPostcode:'TS23 6NQ',
+  opPartnershipAddress:'SUMMER FARM, LONDON ROAD, COMPTON PUGWORTH, NEATON',
+  opFirstName:'',
+  opLastName:'',
+  opContactPosition:'',
+  opContactTelephone:'',
+  opContactEmail:'',
+  opContactEmailConfirm:'',
+  opContactPostcode:'',
+  opContactAddress:'',
+  isFarm:'',
+  isFarmer:'',
+  siteGridRef:'ST5564758136',
+  siteDescription:'',
+  siteAddress:'SUMMER FARM, LONDON ROAD, COMPTON PUGWORTH, NEATON',
+  siteAddressNumber: 'SUMMER FARM',
+  siteAddress1: 'LONDON ROAD',
+  siteAddress2: 'COMPTON PUGWORTH',
+  siteTown: 'NEATON',
+  sitePostcode: 'TS23 6NQ',
+  Exemptions: [
+    'U1',
+    'U10',
+    'D1',
+    'D7'
+  ]
+}
+
 // /start POST action is hard coded to /start-check
 
 // Start new, re-reg or change first page
@@ -32,6 +171,7 @@ router.post('/renew-check', function (req, res) {
   let regNum = req.session.data['registrationNumber']
   var regNumber = regNum.toUpperCase()
   regNumber = regNumber.replace(/\s/g,'')
+  req.session.data.regNumber = regNumber   // store nice format number in data object
 
   var pattern = /^WEX[0-9]{6}$/
 
@@ -51,10 +191,22 @@ router.post('/renew-check', function (req, res) {
     res.redirect('/' + folder + '/registration-number?error=wrongFormat')
 
   } else {
+    // Load sample data #############################
+    // check if entered number exists as sample data
+    if ( samples[regNumber] ){
+      req.session.data = samples[regNumber]
+      req.session.data.selectAddress = samples[regNumber].siteAddress
+    } else {
+      req.session.data = samples.WEX354267   // individual
+      req.session.data.selectAddress = samples.WEX354267.siteAddress
+    }
+    // Then load page
     res.redirect('/' + folder + '/renew-web-check-answers')
   }
 
 })
+
+
 
 // renew-info ================================================
 router.post('/renew-info', function (req, res) {
@@ -272,167 +424,6 @@ router.post('/declaration', function (req, res) {
     "formAction":"renewal-complete"
   })
 })
-
-
-
-// Sample data: company
-const companySampleData = {
-  registerChoice:'startNew',
-  regNumber:'WEX339257',
-  expiryDate:'31 July 2019',
-  countryChoice:'England',
-  appFirstName:'Rachel',
-  appLastName:'Conway',
-  appTelephoneNumber:'0117 978 1234',
-  appEmail:'rachel@ap-email.com',
-  appEmailConfirm:'rachel@ap-email.com',
-  operatorType:'Limited company',
-  opLtdCompanyRegNumber:'AB123456',
-  opLtdCompanyName:'Farm Techniques Ltd',
-  opLtdCompanyPostcode:'BS4 5FT',
-  opLtdCompanyAddress:'WEEB WAY, COLLSTON, TREWBRIDGE',
-  opFirstName:'Bruce',
-  opLastName:'Wallace',
-  opContactPosition:'Site manager',
-  opContactTelephone:'07806 123 456',
-  opContactEmail:'darici@mailinator.net',
-  opContactEmailConfirm:'darici@mailinator.net',
-  opContactPostcode:'BS34 9GH',
-  opContactAddress:'WEEB WAY, COLLSTON, TREWBRIDGE',
-  isFarm:'yes',
-  isFarmer:'yes',
-  siteGridRef:'ST 58132 72695',
-  siteAddress:'WEEB WAY, COLLSTON, TREWBRIDGE',
-  siteDescription:'Lower field, Oak Tree Farm. Parcel ED/1234',
-  siteAddressNumber: 'WEEB WAY',
-  siteAddress1: 'COLLSTON',
-  siteAddress2: '',
-  siteTown: 'TREWBRIDGE',
-  sitePostcode: 'TA17 5RF',
-  Exemptions: [
-    'U1',
-    'U8',
-    'T1',
-    'D7',
-    'S2',
-    'S3'
-  ]
-}
-
-const individualSampleData = {
-  registerChoice:'startNew',
-  regNumber:'WEX354267',
-  expiryDate:'14 July 2019',
-  countryChoice:'England',
-  appFirstName:'Sarah',
-  appLastName:'Williams',
-  appTelephoneNumber:'01342 456123',
-  appEmail:'sarah.williams@email.co.uk',
-  appEmailConfirm:'sarah.williams@email.co.uk',
-  operatorType:'Individual or sole trader',
-  individualName:'Sarah Williams',
-  individualPostcode:'BD45 3UY',
-  individualAddress:'THE OLD FARM, TRUSCOTT, BEDFORDSHIRE',
-  opFirstName:'Sarah',
-  opLastName:'Williams',
-  opContactPosition:'',
-  opContactTelephone:'01761 456123',
-  opContactEmail:'sarah.williams@email.co.uk',
-  opContactEmailConfirm:'sarah.williams@email.co.uk',
-  opContactPostcode:'BD45 3UY',
-  opContactAddress:'THE OLD FARM, TRUSCOTT, BEDFORDSHIRE',
-  isFarm:'yes',
-  isFarmer:'yes',
-  siteGridRef:'ST5320762142',
-  siteDescription:'',
-  siteAddress:'THE OLD FARM, TRUSCOTT, BEDFORDSHIRE',
-  siteAddressNumber: 'THE OLD FARM',
-  siteAddress1: 'TRUSCOTT',
-  siteAddress2: '',
-  siteTown: 'BEDFORDSHIRE',
-  sitePostcode: 'BD45 3UY',
-  Exemptions: [
-    'U1',
-    'U2',
-    'U4',
-    'U8',
-    'U10',
-    'U13',
-    'T1',
-    'D1',
-    'D7',
-    'S2'
-  ]
-}
-
-
-const partnershipSampleData = {
-  registerChoice:'startNew',
-  regNumber:'WEX432123',
-  expiryDate:'7 August 2019',
-  countryChoice:'England',
-  appFirstName:'Brian',
-  appLastName:'Purdy',
-  appTelephoneNumber:'06611456123',
-  appEmail:'b.purdy@email.com',
-  appEmailConfirm:'b.purdy@email.com',
-  operatorType:'Partnership',
-  opPartnershipName: 'Brian Purdy',
-  opPartner1FirstName:'Brian',
-  opPartner1LastName:'Purdy',
-  opPartner2FirstName:'Jane',
-  opPartner2LastName:'Purdy',
-  individualName:'Brian Purdy',
-  opPartnershipPostcode:'TS23 6NQ',
-  opPartnershipAddress:'SUMMER FARM, LONDON ROAD, COMPTON PUGWORTH, NEATON',
-  opFirstName:'Brian',
-  opLastName:'Purdy',
-  opContactPosition:'Partner',
-  opContactTelephone:'06611456123',
-  opContactEmail:'b.purdy@email.com',
-  opContactEmailConfirm:'b.purdy@email.com',
-  opContactPostcode:'TS23 6NQ',
-  opContactAddress:'SUMMER FARM, LONDON ROAD, COMPTON PUGWORTH, NEATON',
-  isFarm:'yes',
-  isFarmer:'no',
-  siteGridRef:'ST5564758136',
-  siteDescription:'',
-  siteAddress:'SUMMER FARM, LONDON ROAD, COMPTON PUGWORTH, NEATON',
-  siteAddressNumber: 'SUMMER FARM',
-  siteAddress1: 'LONDON ROAD',
-  siteAddress2: 'COMPTON PUGWORTH',
-  siteTown: 'NEATON',
-  sitePostcode: 'TS23 6NQ',
-  Exemptions: [
-    'U1',
-    'U10',
-    'D1',
-    'D7'
-  ]
-}
-
-
-
-
-// Load sample data #############################
-router.get('/sample-company', function (req, res) {
-    req.session.data = companySampleData
-    req.session.data.selectAddress = companySampleData.siteAddress
-    res.redirect('/' + folder + '/email-renew')
-})
-
-router.get('/sample-partnership', function (req, res) {
-    req.session.data = partnershipSampleData
-    req.session.data.selectAddress = partnershipSampleData.siteAddress
-    res.redirect('/' + folder + '/email-renew')
-})
-
-router.get('/sample-individual', function (req, res) {
-    req.session.data = individualSampleData
-    req.session.data.selectAddress = individualSampleData.siteAddress
-    res.redirect('/' + folder + '/email-renew')
-})
-
 
 
 
